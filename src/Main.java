d4edf2e936feaad21b1bd79152f9c3bc3bb7a053
I don't now how to programming <98327880+lvl1245@users.noreply.github.com>
2023-05-16 00:31:48 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+      // System.out.println( booleanExpression(true, true, true,false) );
+       // System.out.println( leapYearCount(100) );
+        System.out.println(isPowerOfTwo(17));
+
+    }
+    public static boolean isPowerOfTwo(int value) {
+        return  java.lang.Integer.bitCount(Math.abs(value)) == 1;
+    }
+    public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
+        Boolean[] BoolArr = {a,b,c,d};
+        int TrueNum = 0;
+        for (boolean now : BoolArr){
+            if (now) TrueNum++;
+        }
+
+        return TrueNum == 2 ? true : false;
+    }
+    
+    public static int leapYearCount(int year) {
+        int result = 0;
+        for (int n = 1; n <= year; n++){
+            if (n % 4 == 0 && n % 100 != 0) result++;
+            if (n % 400 == 0) result++;
+        }
+
+        return result;
+    }
+}
